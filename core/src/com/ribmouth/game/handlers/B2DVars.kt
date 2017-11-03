d55944395d9a552dd093b9e25ae011cb08bb5597
Jarno Michiels <wefknrise@gmail.com>
2017-11-03 17:16:33 +0100

    episode 5 + downgraded gdx version (chainShape error) + added ktx lib for box2D + added resources

--- core/src/com/ribmouth/game/handlers/B2DVars.kt
+++ core/src/com/ribmouth/game/handlers/B2DVars.kt
@@ -9,7 +9,9 @@ class B2DVars {
 
         //Category bits (don't use default 1)
         //7 would be 0000 0000 0000 0111
-        const val BIT_GROUND: Short = 2 // 0000 0000 0000 0010
-        const val BIT_PLAYER: Short = 4 // 0000 0000 0000 0100
+        const val BIT_PLAYER: Short = 2 // 0000 0000 0000 0010
+        const val BIT_RED: Short = 4 // 0000 0000 0000 0100
+        const val BIT_GREEN: Short = 8 // 0000 0000 0000 1000
+        const val BIT_BLUE: Short = 16 // 0000 0000 0001 0000
     }
 }
--- core/src/com/ribmouth/game/states/PlayState.kt
+++ core/src/com/ribmouth/game/states/PlayState.kt
@@ -3,6 +3,11 @@ package com.ribmouth.game.states
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType.DynamicBody
@@ -10,13 +15,13 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType.StaticBody
 import com.ribmouth.game.Game.Companion.HEIGHT
 import com.ribmouth.game.Game.Companion.WIDTH
 import com.ribmouth.game.handlers.B2DVars.Companion.BIT_PLAYER
-import com.ribmouth.game.handlers.B2DVars.Companion.BIT_GROUND
+import com.ribmouth.game.handlers.B2DVars.Companion.BIT_RED
 import com.ribmouth.game.handlers.B2DVars.Companion.PPM
 import com.ribmouth.game.handlers.BBInput
 import com.ribmouth.game.handlers.BBInput.Companion.BUTTON1
-import com.ribmouth.game.handlers.BBInput.Companion.BUTTON2
 import com.ribmouth.game.handlers.ContactListener
 import com.ribmouth.game.handlers.GameStateManager
+import ktx.box2d.chain
 
 /**
  * Created by RibMouth on 2/11/2017.
@@ -27,28 +32,23 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
     private val b2dCam: OrthographicCamera = OrthographicCamera()
     private val playerBody: Body
     private val contactListener: ContactListener = ContactListener()
+    private var tileSize: Float = 0f
+
+    //Map loader and renderer
+    private val tileMap: TiledMap = TmxMapLoader().load("maps/level1.tmx")
+    private val tileMapRenderer: OrthogonalTiledMapRenderer = OrthogonalTiledMapRenderer(tileMap)
 
     init {
         world.setContactListener(contactListener)
 
         //Create platform
-        val bDef: BodyDef = BodyDef()
-        bDef.position.set(160f / PPM, 120f / PPM)
+        val bDef = BodyDef()
+        var shape = PolygonShape()
+        val fDef = FixtureDef()
 
         //static body - don't move, unaffected by forces
         //kinematic forces - don't get affected by forces but can move
         //dynamic body - always affected by forces
-        bDef.type = StaticBody
-        var body: Body = world.createBody(bDef)
-
-        //Create fixtures
-        var shape: PolygonShape = PolygonShape()
-        shape.setAsBox(50f / PPM, 5f / PPM)
-        val fDef: FixtureDef = FixtureDef()
-        fDef.shape = shape
-        fDef.filter.categoryBits = BIT_GROUND //Has category bit
-        fDef.filter.maskBits = BIT_PLAYER //Can collide with shapes having these categoryBits
-        body.createFixture(fDef).userData = "ground"
 
         //Create player
         bDef.position.set(160f / PPM, 200f / PPM)
@@ -58,19 +58,51 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
         shape.setAsBox(5f / PPM, 5f / PPM)
         fDef.shape = shape
         fDef.filter.categoryBits = BIT_PLAYER
-        fDef.filter.maskBits = BIT_GROUND
+        fDef.filter.maskBits = BIT_RED
         playerBody.createFixture(fDef).userData = "player"
 
         //Create foot sensor
         shape.setAsBox(2f / PPM, 2f / PPM, Vector2(0f, -5f / PPM), 0f)
         fDef.shape = shape
         fDef.filter.categoryBits = BIT_PLAYER
-        fDef.filter.maskBits = BIT_GROUND
+        fDef.filter.maskBits = BIT_RED
         fDef.isSensor = true
         playerBody.createFixture(fDef).userData = "foot"
 
         //Setup cam
         b2dCam.setToOrtho(false, WIDTH / PPM, HEIGHT / PPM)
+
+        //Map stuff
+        val layer: TiledMapTileLayer = tileMap.layers["red"] as TiledMapTileLayer
+        tileSize = layer.tileWidth
+
+        //Go through all the cells in the layer
+        for (row in 0 until layer.height) {
+            for (col in 0 until layer.width) {
+                //Get cell
+                val cell: Cell = layer.getCell(col, row) ?: continue
+
+                if(cell.tile == null) continue
+
+                //Create body + fixture
+                bDef.type = StaticBody
+                bDef.position.set((col + 0.5f) * tileSize / PPM, (row + 0.5f) * tileSize / PPM)
+
+                //val chainShape = ChainShape()
+                val vector2 = Array<Vector2>(3, { i -> Vector2.Zero })
+
+                vector2[0] = Vector2(-tileSize / 2 / PPM, -tileSize / 2 / PPM) //Bottom left corner
+                vector2[1] = Vector2(-tileSize / 2 / PPM, tileSize / 2 / PPM) //Top left corner
+                vector2[1] = Vector2(tileSize / 2 / PPM, tileSize / 2 / PPM) //Top Right corner
+
+                world.createBody(bDef).chain(vector2[0], vector2[1], vector2[2]) {
+                    friction = 0f
+                    filter.categoryBits = BIT_RED
+                    filter.maskBits = BIT_PLAYER
+                    isSensor = false
+                }
+            }
+        }
     }
 
     override fun handleInput() {
@@ -92,6 +124,11 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
         Gdx.gl20.glClearColor(0.2f, 0.2f, 0.2f, 1f)
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
+        //Draw tileMap
+        tileMapRenderer.setView(cam)
+        tileMapRenderer.render()
+
+        //Draw world
         b2dr.render(world, b2dCam.combined)
     }
 
